23c648c14bcaae756ffb20f0d2d51a8e11360f06
Leif Åstrand <leif@vaadin.com>
2015-01-14 12:52:04 +0200

    Remove deprectated demo code

--- elements-demo/src/main/java/org/vaadin/elements/demo/Demo.java
+++ elements-demo/src/main/java/org/vaadin/elements/demo/Demo.java
@@ -1,22 +1,12 @@
 package org.vaadin.elements.demo;
 
-import java.util.Optional;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-
-import org.vaadin.elements.Element;
-import org.vaadin.elements.Elements;
-import org.vaadin.elements.Root;
-import org.vaadin.elements.TextNode;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.Notification;
 import com.vaadin.ui.TabSheet;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
@@ -52,40 +42,5 @@ public class Demo extends UI {
         layout.setSizeFull();
         layout.setMargin(true);
         setContent(layout);
-        // Root root = ElementIntegration.getRoot(this);
-
-        // demoNewElements(root);
-
-        // demoExistingElements(root);
-
-        // demoPaperComponents(root);
-    }
-
-    private void demoExistingElements(Root root) {
-        Button button = new Button("Test", c -> {
-            root.fetchDom(() -> Notification.show(root.asHtml()));
-        });
-
-        Label label = new Label("Test");
-        VerticalLayout layout = new VerticalLayout(button, label);
-
-        setContent(layout);
-
-        root.fetchDom(() -> {
-            Optional<Element> span = root.querySelector("span > span");
-            span.ifPresent(s -> s.setAttribute("style", "color: blue"));
-        }, layout, button, label);
-    }
-
-    private void demoNewElements(Root root) {
-        root.setInnerHtml("<h1>Hello world</h1>");
-        TextNode text = Elements.createText("Hello");
-        root.appendChild(text);
-
-        Element h1 = root.querySelector("h1").get();
-
-        h1.addEventListener("click", arguments -> {
-            h1.setAttribute("style", "color: blue");
-        });
     }
 }
